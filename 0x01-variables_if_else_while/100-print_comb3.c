90e8add62e7f6866282aa7941b45c4111f3e7236
JoshuaT.Mekuriaw <eshewtadesse@gmail.com>
2022-07-01 05:08:23 -0700

    0

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - prints all the numbers of base 16 in lowercase,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10 && j > i; j ++)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			if (i = 8 && j == 9)
+				break;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}	
