c454681dc21d2c295ed6139ef9df93342258103d
JoshuaT.Mekuriaw <eshewtadesse@gmail.com>
2022-07-01 05:33:30 -0700

    0

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - prints all the numbers of base 16 in lowercase,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 100; i++)
+	{
+	j = 0;
+
+		for (j = 0; j < 100; j++)
+		{
+			if (j > i)
+			{
+				putchar(i/10 + '0');
+				putchar(i%10 + '0');
+				putchar(' ');
+				putchar(j/10 + '0');
+				putchar(j%10 + '0');
+				if (i == 98 && j == 99)
+					break;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
