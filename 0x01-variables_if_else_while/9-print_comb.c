73da0cf185e358460c8b29b565ece522d2c55c3e
JoshuaT.Mekuriaw <eshewtadesse@gmail.com>
2022-07-01 04:49:21 -0700

    0

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,12 +9,14 @@ int main(void)
 {
 	int n;
 
-	for (n = 0; n < 9; n++)
+	for (n = 0; n < 10; n++)
 	{
 		putchar(n + '0');
+		if (n == 9)
+			continue;
 		putchar(',');
 		putchar(' ');
 	}
-	putchar(9 + '0''\n');
+	putchar('\n');
 	return (0);
 }
