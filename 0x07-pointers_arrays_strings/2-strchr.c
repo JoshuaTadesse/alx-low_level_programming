fb4528a7d2598daafdffc850f084c637289fa940
JoshuaT.Mekuriaw <eshewtadesse@gmail.com>
2022-07-18 12:28:22 -0700

    1

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strchr - prints found c
+ * @s: pointer to char
+ * @c: char params to found
+ * Return: *S
+*/
+
+char *_strchr(char *s, char c)
+{
+	return (strchr(s, c));
+}
