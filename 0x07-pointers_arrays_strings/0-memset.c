f31d327bc9870454979bc0f78443b74621c4973c
JoshuaT.Mekuriaw <eshewtadesse@gmail.com>
2022-07-18 10:57:34 -0700

    0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _memset - ironnou
+ * @s: pointer to char params
+ * @b: data to change
+ * @n: index
+ * Return: *s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	return (memset(s, b, n));
+}
