b25af9d87f96fda4f9a98213ba2221744ed92aec
JoshuaT.Mekuriaw <eshewtadesse@gmail.com>
2022-07-18 12:36:12 -0700

    1

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+* _strstr - function locate
+* @haystack: pointer to char
+* @needle: pointer to char
+* Return: 0
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+	return (strstr(haystack, needle);
+}
