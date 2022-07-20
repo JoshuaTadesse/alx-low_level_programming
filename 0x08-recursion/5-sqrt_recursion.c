6c3ba83f5941db90730f04ee0b9ae406ea2d949f
JoshuaT.Mekuriaw <eshewtadesse@gmail.com>
2022-07-20 11:17:20 -0700

    5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -7,12 +7,22 @@
  */
 int _sqrt_recursion(int n)
 {
+	return (_sqrt(n, 1));
+}
+
+/**
+ * _sqrt - _sqrt_recursion
+ * @n: integer paramtr
+ * @i: integer parameter
+ * Return: sqrt
+ */
+int _sqrt(int n, int i)
+{
 	if (n < 0)
 		return (-1);
-
-	double sr = sqrt(n);
-
-	if (sr % 0.1)
+	if ((i * i) > n)
 		return (-1);
-	return (sr);
+	if (i * i == n)
+		return (i);
+	return (_sqrt(n, i + 1));
 }
