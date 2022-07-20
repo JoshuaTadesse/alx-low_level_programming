67cfcb6151118f3f05f937e27f7ecfc6167ab642
JoshuaT.Mekuriaw <eshewtadesse@gmail.com>
2022-07-20 10:22:17 -0700

    0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,8 +10,8 @@ void _puts_recursion(char *s)
 {
 	int i  = 0;
 
-	if (s[i] == '\0')
+	if (*s == '\0')
 		_putchar('\n');
-	_putchar(s[i]);
-	_puts_recursion(s[i++]);
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
